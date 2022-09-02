30be16686cd4783d0c18ca67d0d34bc4759d5158
OxSama <mhmdtajeldin@gmail.com>
2022-09-02 18:22:13 +0200

    tailwind configuring the fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,22 @@ module.exports = {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
-      ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    ],
+    theme: {
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {
+            spacing: {
+                '8xl': '96rem',
+                '9xl': '128rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            }
+        }
+
+    },
+    plugins: [],
 }
