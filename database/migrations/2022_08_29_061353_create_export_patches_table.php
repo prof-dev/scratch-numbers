e15490e50ddf5956872a44bb36036955304babeb
OxSama <mhmdtajeldin@gmail.com>
2022-09-04 14:42:23 +0200

    edited the migrations to make the company id in the batches table

--- database/migrations/2022_08_29_061353_create_export_patches_table.php
+++ database/migrations/2022_08_29_061353_create_export_patches_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('export_patches', function (Blueprint $table) {
+        Schema::create('export_batches', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            // $table->timestamps();
             $table->foreignId("user_id")->constrained();
+            $table->integer('batch_number');
+            $table->timestamp('created_at');
+            $table->foreignId('company_id')->constrained();
         });
     }
 
