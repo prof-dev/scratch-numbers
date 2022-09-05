3ea1eda432194f4b20446362aefc10883b4228d7
OxSama <mhmdtajeldin@gmail.com>
2022-09-05 13:49:36 +0200

    scratch codes page edits

--- resources/views/scratch_codes_batches.blade.php
+++ resources/views/scratch_codes_batches.blade.php
@@ -7,7 +7,7 @@
 
         <div class="w-full px-6 py-8 mx-auto">
             <div class="px-10 py-6 bg-white rounded-lg shadow-xl">
-                <form action="{{ route('create_company') }}" method="POST">
+                <form action="{{ route('create_batch') }}" method="POST">
                     @csrf
                     <div class="row-auto mb-3">
                         <label for="number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Number of Codes') }}</label>
@@ -25,7 +25,7 @@
 
                     <div class="row-auto mb-3">
                         <label for="company" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">{{ __('Company') }}</label>
-                        <select id="company" name="company" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <select id="company" name="company" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option selected>Choose a company</option>
                             @foreach($companies as $company)
                                 @if (old('company') == $company->id)
