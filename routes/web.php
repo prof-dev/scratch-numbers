b4d2af5c7291a8a6563c2ec2dde980f8e9ea4733
OxSama <mhmdtajeldin@gmail.com>
2022-09-06 22:50:35 +0200

    authintication for all routes

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,20 @@ Auth::routes(
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/company', [App\Http\Controllers\CompanyController::class, 'index'])->name('company');
-Route::get('/scratch_codes_batches', [\App\Http\Controllers\ScratchCodesController::class, 'index'])->name('scratch_codes_batches');
-Route::get('/batch_details/{export_patch}', [\App\Http\Controllers\BatchDetailsController::class, 'show'])->name('batch_details');
-Route::get('/batch_details/{export_patch}/export', [\App\Http\Controllers\BatchController::class, 'export'])->name('export_a_batch');
-Route::get('/users' , [\App\Http\Controllers\UserController::class,'index'])->name('users');
 
-Route::post('/company/create', [App\Http\Controllers\CompanyController::class, 'store'])->name('create_company');
-Route::post('/scratch_codes_batches', [\App\Http\Controllers\ScratchCodesController::class, 'store'])->name('create_batch');
-Route::post('/users/create',[\App\Http\Controllers\UserController::class, 'store'])->name('create_user');
+Route::middleware('auth')->group(
+    function(){
+
+        Route::get('/company', [App\Http\Controllers\CompanyController::class, 'index'])->name('company');
+        Route::get('/scratch_codes_batches', [\App\Http\Controllers\ScratchCodesController::class, 'index'])->name('scratch_codes_batches');
+        Route::get('/batch_details/{export_patch}', [\App\Http\Controllers\BatchDetailsController::class, 'show'])->name('batch_details');
+        Route::get('/batch_details/{export_patch}/export', [\App\Http\Controllers\BatchController::class, 'export'])->name('export_a_batch');
+        Route::get('/users' , [\App\Http\Controllers\UserController::class,'index'])->name('users');
+
+        Route::post('/company/create', [App\Http\Controllers\CompanyController::class, 'store'])->name('create_company');
+        Route::post('/scratch_codes_batches', [\App\Http\Controllers\ScratchCodesController::class, 'store'])->name('create_batch');
+        Route::post('/users/create',[\App\Http\Controllers\UserController::class, 'store'])->name('create_user');
+
+
+    }
+);
