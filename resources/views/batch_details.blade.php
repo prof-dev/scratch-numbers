516df6d1d252cef423714d6cd4f36081f05f3aef
OxSama <mhmdtajeldin@gmail.com>
2022-09-12 08:00:19 +0200

    batch details link fix

--- resources/views/batch_details.blade.php
+++ resources/views/batch_details.blade.php
@@ -32,7 +32,7 @@
 
 
                         <div class="flex pt-8 flex-end sm:justify-end sm:pt-0">
-                            <a href="{{ url('/batch_details/'.$exportPatch->id.'/export') }}" class="text-white bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800  rounded-lg text-sm px-4 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 ">
+                            <a id="delete" href="javascript:void(0)" data-id="{{ $exportPatch->id }}" class="text-white bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800  rounded-lg text-sm px-4 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 ">
                                 Export This Batch
                             </a>
                         </div>
