ad73652b6ad59aebfa46932fab8cf93be65ee5ee
OxSama <mhmdtajeldin@gmail.com>
2022-09-12 07:59:54 +0200

    jquery code in js assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 import './bootstrap';
+
+
+
+// delete button click
+$('#delete').click(function() {
+    const id = $(this).data('id');
+
+    if (id) {
+        // const result = window.confirm('Do you want to delete?');
+        // if (result) {
+        $.ajax({
+            url: `/batch_details/${id}/export`,
+            type: 'GET',
+            success: function(response) {
+                if (response && response.status === 'success') {
+                    const data = response.data;
+                    // $(`#post_${data.id}`).remove();
+                }
+            }
+        });
+    } else {
+        console.log('error', 'Post not found');
+    }
+    // }
+});
