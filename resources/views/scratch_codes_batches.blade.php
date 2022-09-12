180b5f662449766f502a522466f38f26bc3d4f27
OxSama <mhmdtajeldin@gmail.com>
2022-09-12 08:01:10 +0200

    scratch code deleting the jquery code from it

--- resources/views/scratch_codes_batches.blade.php
+++ resources/views/scratch_codes_batches.blade.php
@@ -177,30 +177,4 @@
             </div>
     </div>
 </div>
-
-<Script>
-    // delete button click
-    $('#delete').click(function() {
-        const id = $(this).data('id');
-
-        if (id) {
-            // const result = window.confirm('Do you want to delete?');
-            // if (result) {
-                $.ajax({
-                    url: `/batch_details/${id}/export`,
-                    type: 'GET',
-                    success: function(response) {
-                        if (response && response.status === 'success') {
-                            const data = response.data;
-                            // $(`#post_${data.id}`).remove();
-                        }
-                    }
-                });
-            }
-            else {
-                console.log('error', 'Post not found');
-            }
-        // }
-    });
-</Script>
 @endsection
