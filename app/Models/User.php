75f1695961cdf202c7987656200e2cfac6d631b6
OxSama <mhmdtajeldin@gmail.com>
2022-09-13 22:27:26 +0200

    user model relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role(){
+        return $this->belongsTo(Role::class);
+    }
+
+    public function company(){
+        return $this->belongsTo(Company::class);
+    }
 }
