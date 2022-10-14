d8fa086243517c00f73f06367fb342a1f38019a9
OxSama <mhmdtajeldin@gmail.com>
2022-10-14 17:13:43 +0200

    creating base desgin for the dashboard

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Auth::routes(
 );
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 
 Route::middleware('auth')->group(
     function () {
