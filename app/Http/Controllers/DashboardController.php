558dc37bea85cd979f89aa6ac3dcccbdbb342280
Mohamed <mohamedosmanelsayed@yahoo.com>
2022-10-18 23:31:25 +0200

    try calling this

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ScratchCode;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(){
         return view('admin.home_dashboard');
+    }
+
+    public function groupByCompanyBetweenTwoDates(Request $request)
+    {
+        # code...
+        $result=ScratchCode::where("type","SDN")->select(["count(id) as total","sum(status) as activated"])->groupBy("comanyId")->with("company")->get();
+        dd($result);
+        if(isset($request->dateFrom)&&isset($request->dateTo)){
+
+        }
     }
 }
