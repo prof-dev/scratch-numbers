a71a3b2ba4f8d16f38bb3cec66a34b59ef517cad
OxSama <mhmdtajeldin@gmail.com>
2022-10-18 23:09:14 +0200

    changing controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        return view('layouts.dashboard');
+        return view('admin.home_dashboard');
     }
 }
