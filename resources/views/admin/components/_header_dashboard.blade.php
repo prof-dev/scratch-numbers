6363cdbedaaf743ed1ef3f773b95388f8d6f3c2d
OxSama <mhmdtajeldin@gmail.com>
2022-10-23 06:56:35 +0200

    back to website link

--- resources/views/admin/components/_header_dashboard.blade.php
+++ resources/views/admin/components/_header_dashboard.blade.php
@@ -16,10 +16,10 @@
                 </button>
             </div> --}}
             <div class="text-lg font-bold">Dashboard</div>
-            {{-- <div>
-                <button type="button"
+            <div>
+                <a href="{{ route('home') }}"
                     class="flex items-center focus:outline-none rounded-lg text-gray-600 hover:text-yellow-600 focus:text-yellow-600 font-semibold p-2 border border-transparent hover:border-yellow-300 focus:border-yellow-300 transition">
-                    <span class="text-sm">This week</span>
+                    <span class="text-sm">Back to website</span>
                     <span
                         class="inline-flex items-center justify-center w-6 h-6 text-gray-600 text-xs rounded bg-white transition ml-2">
                         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
@@ -28,8 +28,8 @@
                                 d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
                         </svg>
                     </span>
-                </button>
-            </div> --}}
+                </a>
+            </div>
         </div>
     </div>
 </header>
