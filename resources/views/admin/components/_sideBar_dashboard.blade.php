0fe8175304c9b032993ccfadbf8685e16ed92ae6
OxSama <mhmdtajeldin@gmail.com>
2022-10-29 21:44:31 +0200

    code formatting

--- resources/views/admin/components/_sideBar_dashboard.blade.php
+++ resources/views/admin/components/_sideBar_dashboard.blade.php
@@ -8,8 +8,7 @@
             <div class="p-4">
                 <ul class="space-y-1">
                     <li>
-                        <a href="{{ route('dashboard') }}"
-                            class="flex items-center rounded-xl font-bold text-sm py-3 px-4
+                        <a href="{{ route('dashboard') }}" class="flex items-center rounded-xl font-bold text-sm py-3 px-4
                              @if(Route::current()->getName() == 'dashboard')
                              {{ __('bg-yellow-200 text-yellow-900') }}
                              @else {{ __('bg-white hover:bg-yellow-50 text-gray-900') }}
@@ -22,8 +21,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="{{ route('dashboardIndexDate') }}"
-                            class="flex rounded-xl font-bold text-sm py-3 px-4
+                        <a href="{{ route('dashboardIndexDate') }}" class="flex rounded-xl font-bold text-sm py-3 px-4
                             @if(Route::current()->getName() == 'dashboardIndexDate')
                             {{ __('bg-yellow-200 text-yellow-900') }}
                             @else {{ __('bg-white hover:bg-yellow-50 text-gray-900') }}
@@ -32,8 +30,10 @@
                                 class="text-lg mr-4" viewBox="0 0 16 16">
                                 <path
                                     d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z" />
-                                {{-- <path
-                                    d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1z" /> --}}
+                                {{--
+                                <path
+                                    d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1z" />
+                                --}}
                             </svg>Group By Date
                         </a>
                     </li>
@@ -63,15 +63,14 @@
         <div class="p-4">
             <form action="{{ route('logout') }}" method="POST">
                 @csrf
-                <button type="submit"
-                name="logout"
-                class="inline-flex items-center justify-center h-9 px-4 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class=""
-                    viewBox="0 0 16 16">
-                    <path
-                        d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-                </svg>
-            </button> <span class="font-bold text-sm ml-2">Logout</span>
+                <button type="submit" name="logout"
+                    class="inline-flex items-center justify-center h-9 px-4 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class=""
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                    </svg>
+                </button> <span class="font-bold text-sm ml-2">Logout</span>
             </form>
         </div>
     </div>
